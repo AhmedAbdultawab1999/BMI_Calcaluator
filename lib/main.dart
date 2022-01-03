009600d8169bcc99d3ac637c1b3fe69e942b34f6
Ahmed Abdultawab <64033494+AhmedAbdultawab1999@users.noreply.github.com>
2022-01-03 19:51:33 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_learn/bmi_result_screen.dart';
-import 'package:flutter_learn/users_screen.dart';
+import 'package:BMI_Calcaluator/bmi_result_screen.dart';
+import 'package:BMI_Calcaluator/users_screen.dart';
 import 'bmi_screen.dart';
 void  main() {
  runApp(const MyApp()); 
